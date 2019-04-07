4aad9549f07f8c48d28516489219455bb091b345
ceiling12138 <ceiling12138@aliyun.com>
2019-04-07 10:25:02 +0800

    meizuo

---
+++ corejava/src/chaptereight/ActionTest.java
@@ -0,0 +1,20 @@
+package chaptereight;
+
+import javax.swing.*;
+
+/**
+ * @author ceiling
+ * @date 2019/4/7
+ */
+public class ActionTest {
+
+}
+
+class ActionFrame extends JFrame {
+    public ActionFrame() {
+        setTitle("ActionTest");
+        setSize(300,200);
+
+
+    }
+}
