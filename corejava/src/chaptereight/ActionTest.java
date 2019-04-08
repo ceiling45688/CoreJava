c9211b3f488095c6849dd4072111808201bfbea6
ceiling12138 <ceiling12138@aliyun.com>
2019-04-08 11:35:54 +0800

    continued

--- corejava/src/chaptereight/ActionTest.java
+++ corejava/src/chaptereight/ActionTest.java
@@ -1,20 +1,58 @@
 package chaptereight;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 
 /**
  * @author ceiling
  * @date 2019/4/7
  */
 public class ActionTest {
+    public static void main(String[] args){
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                ActionFrame frame = new ActionFrame();
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+        });
+    }
 
 }
 
 class ActionFrame extends JFrame {
+    private JPanel buttonPanel;
     public ActionFrame() {
         setTitle("ActionTest");
         setSize(300,200);
+        buttonPanel = new JPanel();
+//       定义动作
+        Action yellowAction = new ColorAction("YELLOW",new ImageIcon(),Color.YELLOW);
+        Action blueAction = new ColorAction("BLUE",new ImageIcon(),Color.BLUE);
+        Action greenAction = new ColorAction("GREEN",new ImageIcon(),Color.GREEN);
+//      add buttons for actions
+        buttonPanel.add(new JButton(yellowAction));
+        buttonPanel.add(new JButton(blueAction));
+        buttonPanel.add(new JButton(greenAction));
+
+        add(buttonPanel);
 
 
+    }
+
+    public class ColorAction extends AbstractAction {
+        public ColorAction (String name , Icon icon, Color color){
+            putValue(Action.NAME, name);
+            putValue(Action.SMALL_ICON, icon);
+            putValue(Action.SHORT_DESCRIPTION, "set panel color to "+name.toLowerCase());
+            putValue("color",color);
+        }
+
+       @Override public void actionPerformed(ActionEvent event){
+            Color c = (Color)getValue("color");
+            buttonPanel.setBackground(c);
+        }
     }
 }
--- corejava/src/chaptereight/PlafTest.java
+++ corejava/src/chaptereight/PlafTest.java
@@ -6,7 +6,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
- * ps: 只做练习，晚上回来重做一次
+ * @author ceiling
+ * @date 2019/4/8
+ * tips: change the LookAndFeel
  */
 public class PlafTest {
     public static void main(String[] args) {
@@ -28,27 +30,35 @@ class PlafFrame extends JFrame {
         setSize(600, 400);
         buttonPanel = new JPanel();
 
+//        get all of lookandfeel
         UIManager.LookAndFeelInfo[] infos = UIManager.getInstalledLookAndFeels();
-        for (UIManager.LookAndFeelInfo info : infos ){
-            makeButton(info.getName(), info.getClassName());
+        for (UIManager.LookAndFeelInfo info : infos){
+            makeButton (info.getName(), info.getClassName());
         }
+//        add panel
         add(buttonPanel);
     }
 
-    void makeButton(String name, final String plafName){
+    void makeButton(String name, final String classname){
+//        add button to the panel
         JButton button = new JButton(name);
         buttonPanel.add(button);
-
+//        set listener by using inner class
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent event) {
                 try{
-                    UIManager.setLookAndFeel(plafName);
-                    SwingUtilities.updateComponentTreeUI(PlafFrame.this);
+//                  provide lookandfeel name
+                    UIManager.setLookAndFeel(classname);
+//                  update the panel
+                    SwingUtilities.updateComponentTreeUI(buttonPanel);
                 }catch (Exception e){
                     e.printStackTrace();
                 }
             }
         });
+
     }
+
+
 }
